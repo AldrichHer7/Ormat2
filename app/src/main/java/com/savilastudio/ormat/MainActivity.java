19acc679a7bfb4db489c0c3d5538df3ed7303f90
AldrichHer7 <jeroyyane@gmail.com>
2023-12-13 00:12:14 -0600

    fdf

--- app/src/main/java/com/savilastudio/ormat/MainActivity.java
+++ app/src/main/java/com/savilastudio/ormat/MainActivity.java
@@ -5,16 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button iniciarSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iniciarSesion = (Button)findViewById(R.id.iniciarSesion);
+        iniciarSesion.setOnClickListener(this::onClick);
+
     }
-    public void pantallainicio(View view){
-        Intent intent= new Intent(this, PantallaInicial.class);
+    public void onClick(View view){
+        if (view == iniciarSesion){
+            iniciarSesion_OnClick();
+        }
+    }
+
+    public void iniciarSesion_OnClick(){
+        Intent intent = new Intent(this, PantallaInicial.class );
         startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/savilastudio/ormat/PantallaInicial.java
+++ app/src/main/java/com/savilastudio/ormat/PantallaInicial.java
@@ -1,6 +1,9 @@
 package com.savilastudio.ormat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,13 +16,13 @@ import androidx.navigation.ui.NavigationUI;
 import com.savilastudio.ormat.databinding.ActivityPantallaInicialBinding;
 
 public class PantallaInicial extends AppCompatActivity {
+    Button salir;
 
     private ActivityPantallaInicialBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityPantallaInicialBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_pantalla_inicial);
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -29,6 +32,26 @@ public class PantallaInicial extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_pantalla_inicial);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        salir = (Button)findViewById(R.id.salir);
+        salir.setOnClickListener(this::onClick);
+
+
     }
+
+
+    public void onClick(View view){
+        if (view == salir){
+            salir_OnClicK();
+        }
+    }
+
+    public void salir_OnClicK(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 
 }
